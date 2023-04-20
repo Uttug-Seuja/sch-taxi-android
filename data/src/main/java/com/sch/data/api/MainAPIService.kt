a86560513605b948f780086a1f01c841b5069049
JunJangE <wnswkd486@gmail.com>
2023-04-20 15:44:32 +0900

    [sch taxi] data > API 명세 세팅

--- data/src/main/java/com/sch/data/api/MainAPIService.kt
+++ data/src/main/java/com/sch/data/api/MainAPIService.kt
@@ -1,54 +1,198 @@
 package com.sch.data.api
 
-import com.sch.data.model.remote.request.PostRefreshTokenRequest
-import com.sch.data.model.remote.request.PostSignUpRequest
-import com.sch.data.model.remote.request.PostUserProfileRequest
-import com.sch.data.model.remote.response.BaseResponse
-import com.sch.domain.model.ImageUrl
-import com.sch.domain.model.LogInStatus
-import com.sch.domain.model.LoginResponse
-import com.sch.domain.model.UserProfile
+import com.sch.data.model.remote.request.*
+import com.sch.data.model.remote.response.*
+import com.sch.domain.model.*
 import okhttp3.MultipartBody
 import retrofit2.http.*
 
 interface MainAPIService {
+
+    // 등록 요청 <- 1번 false일 경우
+    @POST("/api/v1/credentials/register")
+    suspend fun postRegister(
+        @Query("id_token") idToken: String,
+        @Query("provider") provider: String,
+        @Body body: PostRegisterRequest
+    ): BaseResponse<LoginResponse>
+
     // 토큰 리프래쉬
     @POST("/api/v1/credentials/refresh")
     suspend fun postRefreshToken(@Body body: PostRefreshTokenRequest): BaseResponse<LoginResponse>
 
-    // 로그인(회원인지 판단)
-    @POST("api/v1/user/signIn")
-    suspend fun postLogin(
-        @Query("idTokenString") idTokenString: String
-    ): BaseResponse<LogInStatus>
-
-    // 회원가입
-    @POST("/api/v1/user/signUp")
-    suspend fun postSignUp(
-        @Body body: PostSignUpRequest
-    ): Unit
-
     // 로그아웃
-    @POST("/api/v1/user/logOut")
-    suspend fun postLogOut(): Unit
+    @POST("/api/v1/credentials/logout")
+    suspend fun postLogout(): Unit
+
+    // 로그인 요청 <- 가입한 유저 <- 1번 true일 경우
+    @POST("/api/v1/credentials/login")
+    suspend fun postLogin(
+        @Query("id_token") idToken: String, @Query("provider") provider: String
+    ): BaseResponse<LoginResponse>
+
+    // 토큰 검증 <- 로그인시 제일 처음 1번
+    @GET("/api/v1/credentials/oauth/valid/register")
+    suspend fun getTokenValidation(
+        @Query("id_token") idToken: String, @Query("provider") provider: String
+    ): BaseResponse<IsRegisteredResponse>
+
+    // 회원 탈퇴
+    @DELETE("/api/v1/credentials/me")
+    suspend fun deleteUser(@Query("oauth_access_token") oauth_access_token: String): Unit
 
     // 유저 프로필
-    @GET("/api/v1/user/profile")
+    @GET("/api/v1/users/profile")
     suspend fun getUserProfile(): BaseResponse<UserProfile>
 
+    /***
+    PUT : 자원의 전체 교체, 자원교체 시 모든 필드 필요
+    (만약 전체가 아닌 일부만 전달할 경우,
+    전달한 필드외 모두 null or 초기값 처리되니 주의!!)
+    PATCH : 자원의 부분 교체, 자원교체시 일부 필드 필요
+     * */
+
     // 유저 프로필 변경
-    @POST("/api/v1/user/change")
-    suspend fun postUserProfile(
-        @Body body: PostUserProfileRequest
-    ): Unit
+    @PUT("/api/v1/users/profile")
+    suspend fun putUserProfile(@Body body: PutUserProfileRequest): BaseResponse<UserProfile>
+
+    // 예약 만들기
+    @POST("/api/v1/groups/open")
+    suspend fun postReservation(@Body body: PostReservationRequest): BaseResponse<PostReservationResponse>
+
+    // 예약 수정
+    @PUT("/api/v1/groups/{id}")
+    suspend fun putReservation(
+        @Path("id") id: Int, @Body body: PutReservationRequest
+    ): BaseResponse<PutReservationResponse>
+
+    // 예약 삭제
+    @DELETE("/api/v1/groups/{id}")
+    suspend fun deleteReservation(@Path("id") id: Int): Unit
+
+    // 예약 상세정보
+    suspend fun getReservationDetail(@Path("id") id: Int): BaseResponse<GetReservationDetailResponse>
+
+    // 예약 전체 조회
+    @GET("/api/v1/storages")
+    suspend fun getReservation(
+        @Query("page") page: Int, @Query("size") size: Int, @Query("sort") sort: String?
+    ): BaseResponse<GetPagingReservationResponse>
+
+    // 예약 키워드 검색하기
+    @GET("/api/v1/storages")
+    suspend fun getReservationKeyword(@Body reservationKeyword: GetReservationKeywordRequest): BaseResponse<GetPagingReservationKeywordResponse>
+
+    // 예약 검색하기
+    @GET("/api/v1/storages")
+    suspend fun getReservationSearch(
+        @Body reservationKeyword: GetReservationKeywordRequest,
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+        @Query("sort") sort: String?
+    ): BaseResponse<GetPagingReservationResponse>
+
+    // 참여하기
+    @POST("/api/v1/groups/open")
+    suspend fun postParticipation(@Path("id") id: Int): BaseResponse<PostParticipationResponse>
+
+    // 참여 수정하기
+    @PUT("/api/v1/groups/open")
+    suspend fun putParticipation(@Path("id") id: Int): BaseResponse<PutParticipationResponse>
+
+    // 참여 취소하기
+    @DELETE("/api/v1/groups/open")
+    suspend fun deleteParticipation(@Path("id") id: Int): Unit
+
+    // 내가 예약 했는지 확인
+    @GET("/api/v1/participation")
+    suspend fun getParticipation(@Path("id") id: Int): BaseResponse<GetParticipationResponse>
+
+    // 내 예약 글
+    @GET("/api/v1/storages")
+    suspend fun getUserReservation(
+        @Query("page") page: Int, @Query("size") size: Int, @Query("sort") sort: String?
+    ): BaseResponse<GetPagingReservationResponse>
+
+    // 내가 참여한 예약
+    @GET("/api/v1/storages")
+    suspend fun getUserParticipation(
+        @Query("page") page: Int, @Query("size") size: Int, @Query("sort") sort: String?
+    ): BaseResponse<GetPagingReservationResponse>
+
+    // 야간 푸시알림 설정 <- 마이페이지
+    @POST("/api/v1/options/night")
+    suspend fun postOptionNight(): Unit
+
+    // 야간 푸시알림 설정하제 <- 마이페이지
+    @DELETE("/api/v1/options/night")
+    suspend fun deleteOptionNight(): Unit
+
+    // 새로운 푸시알림 설정 <- 마이페이지
+    @POST("/api/v1/options/new")
+    suspend fun postOptionNew(): Unit
+
+    // 새로운 푸시알림 설정 해제 <- 마이페이지
+    @DELETE("/api/v1/options/new")
+    suspend fun deleteOptionNew(): Unit
+
+    // 유저의 알림 세팅
+    @GET("/api/v1/options")
+    suspend fun getOptions(): BaseResponse<Options>
+
+    // 최신 푸쉬 알림 리스트
+    @GET("/api/v1/notifications")
+    suspend fun getNotifications(): BaseResponse<NotificationList>
+
+    // 푸쉬 알림 보내기
+    @POST("/api/v1/notifications")
+    suspend fun postNotifications(@Body body: PostNotificationRequest): Unit
+
+    // FCM 토큰 등록
+    @POST("/api/v1/notifications/token")
+    suspend fun postNotificationToken(@Body body: PostNotificationTokenRequest): Unit
 
     // 파일 URL로 바꾸기
     @Multipart
-    @POST("/api/v1/images/upload")
+    @POST("/api/v1/images")
     suspend fun postFileToUrl(@Part file: MultipartBody.Part): BaseResponse<ImageUrl>
 
-    // 랜덤 기본 프로필 이미지 가져오기
-    @GET("/api/v1/asset/profile/random")
-    suspend fun getRandomProfileImage(): BaseResponse<ImageUrl>
+    // 그룹에서 나가기
+    @DELETE("/api/v1/groups/{id}/members/leave")
+    suspend fun deleteLeaveGroup(@Path("id") id: Int): BaseResponse<Group>
+
+    // 그룹 가입 요청
+    @POST("/api/v1/groups/{id}/admissions")
+    suspend fun postGroupAdmissions(@Path("id") id: Int): BaseResponse<Admission>
+
+    // 추천 메세지 조회
+    @GET("/api/v1/recommendmessage")
+    suspend fun getRecommendMessage(): BaseResponse<RecommendMessageList>
+
+    // 앱버젼 체크
+    @GET("/api/v1/asset/version")
+    suspend fun getAppVersion(): BaseResponse<AppVersion>
+
+    // 프로필 이미지
+    @GET("/api/v1/asset/profiles")
+    suspend fun getProfiles(): BaseResponse<ProfileList>
+
+    // 프로필 이미지 랜덤
+    @GET("/api/v1/asset/profiles/random")
+    suspend fun getProfilesRandom(): BaseResponse<Profile>
+
+    // 알림 가져오기
+    @GET("/api/v1/alarms")
+    suspend fun getAlarms(): BaseResponse<AlarmList>
+
+    // 알림 있는지 확인
+    @GET("/api/v1/alarms/count")
+    suspend fun getAlarmsCount(): BaseResponse<AlarmCount>
+
+    // 알림 신고하기
+    @POST("/api/v1/reports/notifications/{notification_id}")
+    suspend fun postReportsNotifications(
+        @Path("notification_id") notification_id: Int, @Body body: PostReportsNotificationRequest
+    ): BaseResponse<ReportNotification>
+
 
 }
