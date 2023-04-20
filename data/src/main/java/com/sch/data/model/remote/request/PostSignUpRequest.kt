948335551f10c89b40278f6e938a275b70d70236
JunJangE <wnswkd486@gmail.com>
2023-04-20 15:42:14 +0900

    [sch taxi] data > model 경로 변경

--- data/src/main/java/com/sch/data/model/remote/request/PostSignUpRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.sch.data.model.remote.request
-
-import com.google.gson.annotations.SerializedName
-
-
-data class PostSignUpRequest(
-    @SerializedName("nickname") val nickname: String,
-    @SerializedName("profile_path") val profile_path: String,
-    )
