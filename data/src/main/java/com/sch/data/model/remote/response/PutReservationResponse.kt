ef5de857f761a46234c9db31990738ecd7db6544
JunJangE <wnswkd486@gmail.com>
2023-04-21 16:52:49 +0900

    [sch taxi] data > ProfileResponse 추가

--- data/src/main/java/com/sch/data/model/remote/response/PutReservationResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.sch.data.model.remote.response
-
-import com.google.gson.annotations.SerializedName
-
-data class PutReservationResponse(
-    @SerializedName("reservationId")val reservationId: Int
-)
