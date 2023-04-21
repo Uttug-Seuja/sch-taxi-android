057b8b13de7ebf7682a9d4c13ec3a46b0b48b609
JunJangE <wnswkd486@gmail.com>
2023-04-21 16:59:58 +0900

    [sch taxi] usecase > DeleteUserUseCase 추가

---
+++ domain/src/main/java/com/sch/domain/usecase/remote/DeleteUserUseCase.kt
@@ -0,0 +1,14 @@
+package com.sch.domain.usecase.remote
+
+import com.sch.domain.NetworkResult
+import com.sch.domain.repository.MainRepository
+import javax.inject.Inject
+
+class DeleteUserUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(oauthAccessToken: String): NetworkResult<Unit> =
+        repository.deleteUser(
+            oauthAccessToken = oauthAccessToken
+        )
+}
