70e6b78f0856ceaf26f1d2345595a0379a217c00
JunJangE <wnswkd486@gmail.com>
2023-04-21 17:00:11 +0900

    [sch taxi] usecase > GetUserProfileUseCase 추가

---
+++ domain/src/main/java/com/sch/domain/usecase/remote/GetUserProfileUseCase.kt
@@ -0,0 +1,13 @@
+package com.sch.domain.usecase.remote
+
+import com.sch.domain.NetworkResult
+import com.sch.domain.model.IsRegistered
+import com.sch.domain.model.UserProfile
+import com.sch.domain.repository.MainRepository
+import javax.inject.Inject
+
+class GetUserProfileUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(): NetworkResult<UserProfile> = repository.getUserProfile()
+}
