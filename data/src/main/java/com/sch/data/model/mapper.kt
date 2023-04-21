ad1447cfc508871bb03dc10e6c8775c0ed4ed23f
JunJangE <wnswkd486@gmail.com>
2023-04-21 17:22:12 +0900

    [sch taxi] data >mapper 추가

--- data/src/main/java/com/sch/data/model/mapper.kt
+++ data/src/main/java/com/sch/data/model/mapper.kt
@@ -1,35 +1,8 @@
 package com.sch.data.model
 
 import com.sch.data.model.local.SearchHistoryEntity
-import com.sch.domain.model.SearchHistory
-import com.sch.domain.model.SearchHistoryList
-
-//import com.o2.data.model.response.PagingGroupList
-//import com.sch.domain.model.GroupList
-//import com.sch.domain.model.NotificationContent
-//import com.sch.domain.model.NotificationListContent
-//
-//fun PagingGroupList.toDomain() : GroupList {
-//    return GroupList(
-//        groupContent = groupContent,
-//        last = last
-//    )
-//}
-//
-//fun PagingNotifications.toDomain() : NotificationContent {
-//    return NotificationContent(
-//        content = content,
-//        last = last
-//    )
-//}
-//
-//fun PagingNotificationList.toDomain() : NotificationListContent {
-//    return NotificationListContent(
-////        groups = groups,
-//        reservations = reservations,
-//        notifications = notifications,
-//    )
-//}
+import com.sch.data.model.remote.response.*
+import com.sch.domain.model.*
 
 fun SearchHistory.toData(): SearchHistoryEntity {
     return SearchHistoryEntity(
@@ -37,27 +10,154 @@ fun SearchHistory.toData(): SearchHistoryEntity {
     )
 }
 
-//fun List<SearchHistoryEntity>.toDomain(): List<SearchHistory> {
-//    return map { SearchHistory(
-//        searchHistoryIdx = it.searchHistoryIdx,
-//        title = it.title
-//    ) }
-//}
+fun LoginResponse.toDomain(): Token {
+    return Token(
+        accessToken = this.accessToken, refreshToken = this.refreshToken
+    )
+}
+
+fun IsRegisteredResponse.toDomain(): IsRegistered {
+    return IsRegistered(
+        isRegistered = this.isRegistered
+    )
+}
+
+fun UserProfileResponse.toDomain(): UserProfile {
+    return UserProfile(
+        email = email, id = id, nickname = nickname, profilePath = profilePath
+    )
+}
 
 fun List<SearchHistoryEntity>.toDomain(): SearchHistoryList {
-    return SearchHistoryList(
+    return SearchHistoryList(map {
+        SearchHistory(
+            searchHistoryIdx = it.searchHistoryIdx, title = it.title
+        )
+    })
+}
+
+fun SearchHistoryEntity.toDomain(): SearchHistory {
+    return SearchHistory(
+        searchHistoryIdx = this.searchHistoryIdx, title = this.title
+    )
+}
+
+fun ReservationResponse.toDomain(): Reservation {
+    return Reservation(
+        id = this.id,
+        title = this.title,
+        reservationTime = this.reservationTime,
+        startingPlace = this.startingPlace,
+        destination = this.destination,
+        sex = this.sex,
+        reservationStatus = this.reservationStatus
+    )
+}
+
+fun ReservationDetailResponse.toDomain(): ReservationDetail {
+    return ReservationDetail(
+        id = id,
+        title = title,
+        reserveDate = reserveDate,
+        reserveTime = reserveTime,
+        startingPlace = startingPlace,
+        destination = destination,
+        sex = sex,
+        userSex = userSex,
+        createdAt = createdAt,
+        currentNum = currentNum,
+        passengerNum = passengerNum,
+        challengeWord = challengeWord,
+        countersignWord = countersignWord,
+        startLatitude = startLatitude,
+        startLongitude = startLongitude,
+        finishLatitude = finishLatitude,
+        finishLongitude = finishLongitude,
+        reservationStatus = reservationStatus,
+        userUid = userUid,
+        name = name,
+        schoolNum = schoolNum
+    )
+}
+
+fun PagingReservationsResponse.toDomain(): PagingReservations {
+    return PagingReservations(content = this.content, last = this.last)
+}
+
+fun ReservationKeywordResponse.toDomain(): ReservationKeyword {
+    return ReservationKeyword(keywordId = this.keywordId, keywordTitle = this.keywordTitle)
+}
+
+fun ParticipationResponse.toDomain(): Participation {
+    return Participation(reservationId = this.reservationId, isParticipation = isParticipation)
+}
+
+fun OptionsResponse.toDomain(): Options {
+    return Options(
+        new_option = this.new_option,
+        reaction_option = reaction_option,
+        night_option = night_option
+    )
+}
+
+fun List<NotificationResponse>.toDomain(): NotificationList {
+    return NotificationList(
         map {
-            SearchHistory(
-                searchHistoryIdx = it.searchHistoryIdx,
+            Notification(
+                content = it.content,
+                created_date = it.created_date,
+                image_url = it.image_url,
+                notification_id = it.notification_id,
+                send_user_id = it.send_user_id,
                 title = it.title
+            )
+
+        }
+
+    )
+}
+
+fun ImageUrlResponse.toDomain(): ImageUrl {
+    return ImageUrl(image_url = this.image_url)
+}
+
+fun List<ProfileResponse>.toDomain(): ProfileList {
+    return ProfileList(
+        map {
+            Profile(
+                id = it.id,
+                title = it.title,
+                url = it.url
             )
         }
     )
 }
 
-fun SearchHistoryEntity.toDomain(): SearchHistory {
-    return SearchHistory(
-        searchHistoryIdx = this.searchHistoryIdx,
-        title = this.title
+fun ProfileResponse.toDomain(): Profile {
+    return Profile(id = this.id, title = this.title, url = this.url)
+}
+
+fun List<AlarmResponse>.toDomain(): AlarmList {
+    return AlarmList(
+        map {
+            Alarm(
+                content = it.content,
+                created_at = it.created_at,
+                title = it.title,
+                type = it.type,
+                user_id = it.user_id,
+                user_profile = it.user_profile
+            )
+        }
+    )
+}
+
+fun ReportNotificationResponse.toDomain(): ReportNotification {
+    return ReportNotification(
+        id = this.id,
+        notification_id = this.notification_id,
+        report_reason = this.report_reason,
+        description = this.description,
+        defendant_id = this.defendant_id
     )
 }
