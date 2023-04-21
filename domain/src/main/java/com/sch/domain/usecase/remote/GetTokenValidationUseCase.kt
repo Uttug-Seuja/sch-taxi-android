f55e7d9f151340a0a0ab52352aca68bbc88c02f2
JunJangE <wnswkd486@gmail.com>
2023-04-21 17:00:05 +0900

    [sch taxi] usecase > GetTokenValidationUseCase 추가

---
+++ domain/src/main/java/com/sch/domain/usecase/remote/GetTokenValidationUseCase.kt
@@ -0,0 +1,16 @@
+package com.sch.domain.usecase.remote
+
+import com.sch.domain.NetworkResult
+import com.sch.domain.model.IsRegistered
+import com.sch.domain.repository.MainRepository
+import javax.inject.Inject
+
+class GetTokenValidationUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(idToken: String, provider: String): NetworkResult<IsRegistered> =
+        repository.getTokenValidation(
+            idToken = idToken,
+            provider = provider
+        )
+}
