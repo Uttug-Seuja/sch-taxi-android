b3bab87ec391482a0d7ec840ebc418d07d573df2
JunJangE <wnswkd486@gmail.com>
2023-04-21 17:03:51 +0900

    [sch taxi] data > 파일명 변경

--- data/src/main/java/com/sch/data/model/remote/response/GetPagingReservationResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.sch.data.model.remote.response
-
-import com.google.gson.annotations.SerializedName
-
-data class GetPagingReservationResponse(
-    @SerializedName("pagingReservations") val pagingReservations: PagingReservations
-)
--- data/src/main/java/com/sch/data/model/remote/response/GetParticipationResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.sch.data.model.remote.response
-
-import com.google.gson.annotations.SerializedName
-
-data class GetParticipationResponse(
-    @SerializedName("isParticipation") val isParticipation: Boolean
-)
