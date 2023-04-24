7d2556bec3f61b29737db65095a7b6f24232fa6a
JunJangE <wnswkd486@gmail.com>
2023-04-25 01:16:28 +0900

    [sch taxi] data > base url 설정

--- data/src/main/java/com/sch/data/api/ApiClient.kt
+++ data/src/main/java/com/sch/data/api/ApiClient.kt
@@ -3,6 +3,6 @@ package com.sch.data.api
 import com.sch.data.BuildConfig
 
 object ApiClient {
-    const val BASE_URL = ""
+    const val BASE_URL = BuildConfig.BASE_URL
     const val KAKAO_BASE_URL = "https://dapi.kakao.com/"
 }
