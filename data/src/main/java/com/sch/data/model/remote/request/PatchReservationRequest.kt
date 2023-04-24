757cf3a171e6c71164202245556f6cb73b0c8670
JunJangE <wnswkd486@gmail.com>
2023-04-25 01:17:32 +0900

    [sch taxi] data > reservation 업데이트 응답 요청 변경

---
+++ data/src/main/java/com/sch/data/model/remote/request/PatchReservationRequest.kt
@@ -0,0 +1,15 @@
+package com.sch.data.model.remote.request
+
+import com.google.gson.annotations.SerializedName
+
+
+data class PatchReservationRequest(
+    @SerializedName("title") val title: String,
+    @SerializedName("startPoint") val startPoint: String,
+    @SerializedName("destination") val destination: String,
+    @SerializedName("departureDate") val departureDate: String,
+    @SerializedName("startLatitude") val startLatitude: Double,
+    @SerializedName("startLongitude") val startLongitude: Double,
+    @SerializedName("destinationLatitude") val destinationLatitude: Double,
+    @SerializedName("destinationLongitude") val destinationLongitude: Double,
+)
