ca4fdf0e65fde23409aec2d6fa4530016cfe41b5
JunJangE <wnswkd486@gmail.com>
2023-04-25 13:03:29 +0900

    [sch taxi] usecase > GetReservationSearchUseCase 추가

---
+++ domain/src/main/java/com/sch/domain/usecase/remote/GetReservationSearchUseCase.kt
@@ -0,0 +1,26 @@
+package com.sch.domain.usecase.remote
+
+import com.sch.domain.NetworkResult
+import com.sch.domain.model.PagingReservations
+import com.sch.domain.model.Reservation
+import com.sch.domain.model.ResultSearchKeyword
+import com.sch.domain.model.Token
+import com.sch.domain.repository.KakaoRepository
+import com.sch.domain.repository.MainRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class GetReservationSearchUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(
+        keyword: String,
+        page: Int,
+        size: Int
+    ): NetworkResult<PagingReservations> =
+        repository.getReservationSearch(
+            keyword = keyword,
+            page = page,
+            size = size,
+        )
+}
