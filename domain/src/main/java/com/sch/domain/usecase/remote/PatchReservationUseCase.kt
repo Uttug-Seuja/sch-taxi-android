28c8e39f4d08e68c5fd84f6b6bc5a052bb0de78f
JunJangE <wnswkd486@gmail.com>
2023-04-25 13:04:08 +0900

    [sch taxi] usecase > PatchReservationUseCase 추가

---
+++ domain/src/main/java/com/sch/domain/usecase/remote/PatchReservationUseCase.kt
@@ -0,0 +1,43 @@
+package com.sch.domain.usecase.remote
+
+import com.sch.domain.NetworkResult
+import com.sch.domain.model.Reservation
+import com.sch.domain.model.ResultSearchKeyword
+import com.sch.domain.model.Token
+import com.sch.domain.repository.KakaoRepository
+import com.sch.domain.repository.MainRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class PatchReservationUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(
+        reservationId: Int,
+        title: String,
+        startPoint: String,
+        destination: String,
+        departureDate: String,
+        gender: String,
+        passengerNum: String,
+        currentNum: String,
+        startLatitude: Double,
+        startLongitude: Double,
+        destinationLatitude: Double,
+        destinationLongitude: Double
+    ): NetworkResult<Reservation> =
+        repository.patchReservation(
+            reservationId = reservationId,
+            title = title,
+            startPoint = startPoint,
+            destination = destination,
+            departureDate = departureDate,
+            gender = gender,
+            passengerNum = passengerNum,
+            currentNum = currentNum,
+            startLatitude = startLatitude,
+            startLongitude = startLongitude,
+            destinationLatitude = destinationLatitude,
+            destinationLongitude = destinationLongitude
+        )
+}
