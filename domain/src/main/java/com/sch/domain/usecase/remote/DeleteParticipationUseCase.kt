dfcc75d0b7362e69e9adb3718d99ac01b0b2c550
JunJangE <wnswkd486@gmail.com>
2023-04-25 12:56:36 +0900

    [sch taxi] usecase > DeleteParticipationUseCase 추가

---
+++ domain/src/main/java/com/sch/domain/usecase/remote/DeleteParticipationUseCase.kt
@@ -0,0 +1,18 @@
+package com.sch.domain.usecase.remote
+
+import com.sch.domain.NetworkResult
+import com.sch.domain.model.Participation
+import com.sch.domain.model.Reservation
+import com.sch.domain.model.ResultSearchKeyword
+import com.sch.domain.model.Token
+import com.sch.domain.repository.KakaoRepository
+import com.sch.domain.repository.MainRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class DeleteParticipationUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(id: Int): NetworkResult<Unit> =
+        repository.deleteParticipation(id = id)
+}
