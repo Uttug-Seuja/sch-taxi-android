882e5a1221d9848b3bde851ec4bc290c9630d39c
JunJangE <wnswkd486@gmail.com>
2023-04-25 13:04:24 +0900

    [sch taxi] usecase > PostParticipationUseCase 추가

---
+++ domain/src/main/java/com/sch/domain/usecase/remote/PostParticipationUseCase.kt
@@ -0,0 +1,18 @@
+package com.sch.domain.usecase.remote
+
+import com.sch.domain.NetworkResult
+import com.sch.domain.model.Participation
+import com.sch.domain.model.Reservation
+import com.sch.domain.model.ResultSearchKeyword
+import com.sch.domain.model.Token
+import com.sch.domain.repository.KakaoRepository
+import com.sch.domain.repository.MainRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class PostParticipationUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(id: Int, seatPosition: String): NetworkResult<Participation> =
+        repository.postParticipation(id = id, seatPosition = seatPosition)
+}
