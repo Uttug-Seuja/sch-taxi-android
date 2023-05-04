f2577522dbc5d8bc632a15c1618bf90795bb2f82
JunJangE <wnswkd486@gmail.com>
2023-05-04 11:43:02 +0900

    [sch taxi] model > UserInfoResponse 추가

---
+++ data/src/main/java/com/sch/data/model/remote/response/UserInfoResponse.kt
@@ -0,0 +1,13 @@
+package com.sch.data.model.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class UserInfoResponse(
+    @SerializedName("userId") val userId: Int,
+    @SerializedName("schoolNum") val schoolNum: String,
+    @SerializedName("gender") val gender: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("profilePath") val profilePath: String,
+    @SerializedName("currentTemperature") val currentTemperature: Double
+)
