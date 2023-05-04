2e1d59aad47dd58e949c45fdf3092633133e63ba
JunJangE <wnswkd486@gmail.com>
2023-05-04 11:57:25 +0900

    [sch taxi] model > UserProfile -> UserInfo로 변경

--- domain/src/main/java/com/sch/domain/usecase/PatchUserProfileUseCase.kt
+++ domain/src/main/java/com/sch/domain/usecase/PatchUserProfileUseCase.kt
@@ -1,14 +1,14 @@
 package com.sch.domain.usecase
 
 import com.sch.domain.NetworkResult
-import com.sch.domain.model.UserProfile
+import com.sch.domain.model.UserInfo
 import com.sch.domain.repository.MainRepository
 import javax.inject.Inject
 
 class PatchUserProfileUseCase @Inject constructor(
     private val repository: MainRepository
 ) {
-    suspend operator fun invoke(profilePath: String): NetworkResult<UserProfile> =
+    suspend operator fun invoke(profilePath: String): NetworkResult<UserInfo> =
         repository.patchUserProfile(
             profilePath = profilePath
         )
