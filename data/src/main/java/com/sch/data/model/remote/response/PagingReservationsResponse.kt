fe24232ed5c44ece5fe2641d9a1465e478431c38
JunJangE <wnswkd486@gmail.com>
2023-05-04 10:58:42 +0900

    [sch taxi] model > 불필요한 파일 삭제

--- data/src/main/java/com/sch/data/model/remote/response/PagingReservationsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.sch.data.model.remote.response
-
-import com.google.gson.annotations.SerializedName
-import com.sch.domain.model.Reservation
-
-data class PagingReservationsResponse(
-    @SerializedName("content") val content: List<Reservation>,
-    @SerializedName("last") val last: Boolean,
-)
--- domain/src/main/java/com/sch/domain/model/PagingReservations.kt
+++
@@ -1,7 +0,0 @@
-package com.sch.domain.model
-
-
-data class PagingReservations(
-    val content: List<Reservation>,
-    val last: Boolean,
-)
