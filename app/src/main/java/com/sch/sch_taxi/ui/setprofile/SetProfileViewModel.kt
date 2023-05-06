6f17f740509963389e730af118366daa5e4a0ac5
JunJangE <wnswkd486@gmail.com>
2023-05-06 14:53:13 +0900

    [sch taxi] 프로필 설정 페이지 > getUserProfileUseCase, postRegisterUseCase,  postFileToImageUseCase 추가

--- app/src/main/java/com/sch/sch_taxi/ui/setprofile/SetProfileViewModel.kt
+++ app/src/main/java/com/sch/sch_taxi/ui/setprofile/SetProfileViewModel.kt
@@ -2,7 +2,13 @@ package com.sch.sch_taxi.ui.setprofile
 
 import android.util.Log
 import com.sch.domain.model.Profile
+import com.sch.domain.onSuccess
+import com.sch.domain.usecase.main.GetUserProfileUseCase
+import com.sch.domain.usecase.main.PostFileToImageUseCase
+import com.sch.domain.usecase.main.PostRegisterUseCase
 import com.sch.sch_taxi.base.BaseViewModel
+import com.sch.sch_taxi.di.PresentationApplication.Companion.editor
+import com.sch.sch_taxi.di.PresentationApplication.Companion.sSharedPreferences
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -11,12 +17,17 @@ import okhttp3.MultipartBody
 
 @HiltViewModel
 class SetProfileViewModel @Inject constructor(
+    private val getUserProfileUseCase: GetUserProfileUseCase,
+    private val postRegisterUseCase: PostRegisterUseCase,
+    private val postFileToImageUseCase: PostFileToImageUseCase
 ) : BaseViewModel(), SetProfileActionHandler {
 
     private val TAG = "SetProfileViewModel"
 
-    private val _navigationHandler: MutableSharedFlow<SetProfileNavigationAction> = MutableSharedFlow<SetProfileNavigationAction>()
-    val navigationHandler: SharedFlow<SetProfileNavigationAction> = _navigationHandler.asSharedFlow()
+    private val _navigationHandler: MutableSharedFlow<SetProfileNavigationAction> =
+        MutableSharedFlow<SetProfileNavigationAction>()
+    val navigationHandler: SharedFlow<SetProfileNavigationAction> =
+        _navigationHandler.asSharedFlow()
 
     var inputContent = MutableStateFlow<String>("")
     var editTextMessageCountEvent = MutableStateFlow<Int>(0)
@@ -34,10 +45,16 @@ class SetProfileViewModel @Inject constructor(
     init {
         baseViewModelScope.launch {
             showLoading()
-//            mainRepository.getProfilesRandom()
-//                .onSuccess { profile ->
-                    profileImg.emit(Profile(1,"ddd","https://cdn-icons-png.flaticon.com/512/10089/10089718.png"))
-//                }
+            getUserProfileUseCase()
+                .onSuccess { profile ->
+                    profileImg.emit(
+                        Profile(
+                            1,
+                            "ddd",
+                            "https://cdn-icons-png.flaticon.com/512/10089/10089718.png"
+                        )
+                    )
+                }
             dismissLoading()
         }
 
@@ -51,16 +68,6 @@ class SetProfileViewModel @Inject constructor(
             ageInputContent.debounce(0).collectLatest {
                 onAgeEditTextCount(it.length)
             }
-        }
-        baseViewModelScope.launch {
-            showLoading()
-//            mainRepository.getUserProfile()
-//                .onSuccess {
-//                    beforeProfile = it
-//                    profileImg.emit(it.profile_path)
-//                    profileName.emit(it.nickname)
-//                }
-            dismissLoading()
         }
     }
 
@@ -77,21 +84,21 @@ class SetProfileViewModel @Inject constructor(
         }
     }
 
-    override fun onGenderManClicked(){
+    override fun onGenderManClicked() {
         baseViewModelScope.launch {
             isManEvent.value = true
         }
 
     }
 
-    override fun onGenderWomanClicked(){
+    override fun onGenderWomanClicked() {
         baseViewModelScope.launch {
             Log.d("ttt", "누름")
             isManEvent.value = false
         }
     }
 
-    override fun onAgeSetClicked(){
+    override fun onAgeSetClicked() {
         baseViewModelScope.launch {
             _navigationHandler.emit(SetProfileNavigationAction.NavigateToAgeNumberPicker)
         }
@@ -108,10 +115,10 @@ class SetProfileViewModel @Inject constructor(
     fun setFileToUri(file: MultipartBody.Part) {
         baseViewModelScope.launch {
             showLoading()
-//            mainRepository.postFileToUrl(file = file)
-//                .onSuccess {
+            postFileToImageUseCase(file = file)
+                .onSuccess {
 //                    profileImg.value = it.image_url
-//                }
+                }
             dismissLoading()
         }
     }
@@ -119,30 +126,32 @@ class SetProfileViewModel @Inject constructor(
     override fun onSelectionDoneClicked() {
         baseViewModelScope.launch {
             showLoading()
-//            val idToken = sSharedPreferences.getString("id_token", null)
-//            val provider = sSharedPreferences.getString("provider", null)
-//            if(inputContent.value == "") {
-//                _navigationHandler.emit(SetProfileNavigationAction.NavigateToEmpty)
-//            } else {
-//                if(idToken != null && provider != null) {
-//                    mainRepository.postRegister(
-//                        idToken = idToken,
-//                        provider = provider,
-//                        profile_path = profileImg.value!!.url,
-//                        nickname = inputContent.value
-//                    ).onSuccess {
-//                        editor.putString("access_token", it.access_token)
-//                        editor.putString("refresh_token", it.refresh_token)
-//                        editor.commit()
-//                        val deviceId = sSharedPreferences.getString("device_id", null)
-//                        val fcmToken = sSharedPreferences.getString("fcm_token", null)
-//                        mainRepository.postNotificationToken(device_id = deviceId!!, token = fcmToken!!)
+            val idToken = sSharedPreferences.getString("idToken", null)
+            val provider = sSharedPreferences.getString("provider", null)
+            if (inputContent.value == "") {
+                _navigationHandler.emit(SetProfileNavigationAction.NavigateToEmpty)
+            } else {
+                if (idToken != null && provider != null) {
+                    postRegisterUseCase(
+                        idToken = idToken,
+                        provider = provider,
+                        name = inputContent.value,
+                        schoolNum = inputContent.value,
+                        gender = inputContent.value,
+                        profilePath = profileImg.value!!.url,
+                    ).onSuccess {
+                        editor.putString("accessToken", it.accessToken)
+                        editor.putString("refreshToken", it.refreshToken)
+                        editor.commit()
+                        val deviceId = sSharedPreferences.getString("deviceId", null)
+                        val fcmToken = sSharedPreferences.getString("fcmToken", null)
+//                        postNotificationTokenUseCase(deviceId = deviceId!!, token = fcmToken!!)
 //                            .onSuccess {
-                                _navigationHandler.emit(SetProfileNavigationAction.NavigateToHome)
+//                                _navigationHandler.emit(SetProfileNavigationAction.NavigateToHome)
 //                            }
-//                    }
-//                }
-//            }
+                    }
+                }
+            }
             dismissLoading()
         }
     }
