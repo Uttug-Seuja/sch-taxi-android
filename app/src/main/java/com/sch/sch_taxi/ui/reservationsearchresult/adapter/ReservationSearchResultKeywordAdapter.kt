bf97d9cb25de4d3444c2ca860050ebdafe7d20b8
JunJangE <wnswkd486@gmail.com>
2023-05-08 17:23:17 +0900

    [sch taxi] 예약 검색 결과 페이지 > ReservationSearchResultKeywordAdapter 추가

---
+++ app/src/main/java/com/sch/sch_taxi/ui/reservationsearchresult/adapter/ReservationSearchResultKeywordAdapter.kt
@@ -0,0 +1,52 @@
+package com.sch.sch_taxi.ui.reservationsearchresult.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.sch.domain.model.Keyword
+import com.sch.sch_taxi.databinding.HolderReservationSearchBinding
+import com.sch.sch_taxi.ui.reservationsearchresult.ReservationSearchResultActionHandler
+
+class ReservationSearchResultKeywordAdapter(
+    private val eventListener: ReservationSearchResultActionHandler,
+) : PagingDataAdapter<Keyword, ReservationSearchResultKeywordAdapter.ViewHolder>(ReservationSearchItemDiffCallback){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            HolderReservationSearchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                .apply {
+                    resultEventListener = this@ReservationSearchResultKeywordAdapter.eventListener
+                }
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        getItem(position)?.let { item ->
+            holder.bind(item)
+        }
+    }
+
+    class ViewHolder(
+        private val binding: HolderReservationSearchBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Keyword) {
+            binding.apply {
+                holder = item
+                executePendingBindings()
+            }
+        }
+    }
+
+
+
+    internal object ReservationSearchItemDiffCallback : DiffUtil.ItemCallback<Keyword>() {
+        override fun areItemsTheSame(oldItem: Keyword, newItem: Keyword) =
+            oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: Keyword, newItem: Keyword) =
+            oldItem.equals(newItem)
+    }
+}
