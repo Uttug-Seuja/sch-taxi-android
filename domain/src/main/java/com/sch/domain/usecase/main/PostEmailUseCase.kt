dc71fb53f9be95d2f8c5fe60460951a2b283f22b
JunJangE <wnswkd486@gmail.com>
2023-05-10 17:27:34 +0900

    [sch taxi] 프로필 설정 페이지 > PostEmailUseCase 추가

---
+++ domain/src/main/java/com/sch/domain/usecase/main/PostEmailUseCase.kt
@@ -0,0 +1,14 @@
+package com.sch.domain.usecase.main
+
+import com.sch.domain.NetworkResult
+import com.sch.domain.model.ImageUrl
+import com.sch.domain.repository.MainRepository
+import okhttp3.MultipartBody
+import javax.inject.Inject
+
+class PostEmailUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(email : String): NetworkResult<Unit> =
+        repository.postEmail(email = email)
+}
