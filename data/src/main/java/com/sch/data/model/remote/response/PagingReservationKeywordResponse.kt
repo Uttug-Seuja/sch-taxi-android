0515d6f45bb704edbcf74d7b6823fbb1a1fe0e43
JunJangE <wnswkd486@gmail.com>
2023-05-11 14:46:37 +0900

    [sch taxi] 예약 검색 페이지 > 추천 검색어 로직 추가

--- data/src/main/java/com/sch/data/model/remote/response/PagingReservationKeywordResponse.kt
+++ data/src/main/java/com/sch/data/model/remote/response/PagingReservationKeywordResponse.kt
@@ -3,6 +3,6 @@ package com.sch.data.model.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class PagingReservationKeywordResponse(
-    @SerializedName("content") val content: List<String>,
+    @SerializedName("content") val content: List<KeywordResponse>,
     @SerializedName("last") val last: Boolean
 )
