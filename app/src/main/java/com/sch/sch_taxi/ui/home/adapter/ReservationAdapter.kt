1f4b8611a08e911174905d01c9381c7ed24c0e56
junjange <wnswkd486@naver.com>
2023-05-15 11:04:13 +0900

    [sch taxi] 홈 페이지 > API - UI 매핑

--- app/src/main/java/com/sch/sch_taxi/ui/home/adapter/ReservationAdapter.kt
+++ app/src/main/java/com/sch/sch_taxi/ui/home/adapter/ReservationAdapter.kt
@@ -1,5 +1,6 @@
 package com.sch.sch_taxi.ui.home.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
@@ -11,7 +12,7 @@ import com.sch.sch_taxi.ui.home.HomeActionHandler
 
 class ReservationAdapter(
     private val eventListener: HomeActionHandler,
-) : PagingDataAdapter<Reservation, ReservationAdapter.ViewHolder>(ReservationItemDiffCallback){
+) : PagingDataAdapter<Reservation, ReservationAdapter.ViewHolder>(ReservationItemDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -32,14 +33,38 @@ class ReservationAdapter(
         private val binding: HolderReservationBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
+        private val gender =
+            hashMapOf<String, String>("ALL" to "남녀모두", "MAN" to "남자만", "WOMAN" to "여자만")
+        private val reserveStatus =
+            hashMapOf<String, String>(
+                "POSSIBLE" to "신청가능",
+                "IMMINENT" to "마감임박!",
+                "DEADLINE" to "마감"
+            )
+        private val stateTextColor = hashMapOf<String, String>(
+            "POSSIBLE" to "#FFFFFF",
+            "IMMINENT" to "#FFFFFF",
+            "DEADLINE" to "#cccccc"
+        )
+        private val stateBtnColor = hashMapOf<String, String>(
+            "POSSIBLE" to "#1570ff",
+            "IMMINENT" to "#FF4D37",
+            "DEADLINE" to "#EEEEEE"
+        )
+
         fun bind(item: Reservation) {
             binding.apply {
                 holder = item
+                startTimeText.text = item.departureDate.substring(11, 16)
+                binding.subTitleText.text =
+                    "${item.startPoint}->${item.destination}•${gender[item.gender]}"
+                binding.stateBtn.setCardBackgroundColor(Color.parseColor(stateBtnColor[item.reservationStatus]))
+                binding.stateText.text = reserveStatus[item.reservationStatus]
+                binding.stateText.setTextColor(Color.parseColor(stateTextColor[item.reservationStatus]))
                 executePendingBindings()
             }
         }
     }
-
 
 
     internal object ReservationItemDiffCallback : DiffUtil.ItemCallback<Reservation>() {
