41d48b92ee26d046f1bdd507aa5659fce64fea19
JunJangE <wnswkd486@gmail.com>
2023-05-24 17:48:30 +0900

    [sch taxi] 예약 게시글 상세 페이지 > HostInfo 추가

---
+++ domain/src/main/java/com/sch/domain/model/HostInfo.kt
@@ -0,0 +1,10 @@
+package com.sch.domain.model
+
+
+data class HostInfo(
+    val userId: Int,
+    val gender: String,
+    val name: String,
+    val email: String,
+    val profilePath: String,
+)
