33a9af289c8d62d146ca69858ba3069d74be342d
JunJangE <wnswkd486@gmail.com>
2023-05-24 17:48:40 +0900

    [sch taxi] 예약 게시글 상세 페이지 > HostInfoResponse 추가

---
+++ data/src/main/java/com/sch/data/model/remote/response/HostInfoResponse.kt
@@ -0,0 +1,12 @@
+package com.sch.data.model.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class HostInfoResponse(
+    @SerializedName("userId") val userId: Int,
+    @SerializedName("schoolNum") val schoolNum: String,
+    @SerializedName("gender") val gender: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("profilePath") val profilePath: String,
+)
