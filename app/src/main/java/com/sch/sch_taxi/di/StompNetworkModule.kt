2bda02d6c7a1ea4cbdd07d94c2287830da72e51d
JunJangE <wnswkd486@gmail.com>
2023-08-23 16:55:51 +0900

    [sch taxi] 홈 페이지 > stomp 연결

--- app/src/main/java/com/sch/sch_taxi/di/StompNetworkModule.kt
+++ app/src/main/java/com/sch/sch_taxi/di/StompNetworkModule.kt
@@ -7,49 +7,54 @@ import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import okio.ByteString
+import ua.naiksoftware.stomp.Stomp
 
-object StompNetworkModule {
+// https://gwynn.tistory.com/89
+class StompNetworkModule {
     private val client = OkHttpClient()
-    private val request = Request.Builder().url(BASE_URL).build()
+    private val url = "ws" + BASE_URL.substring(4) + "stomp/chat"
+    private val request = Request.Builder().url(url).build()
 
-    private val listener = object : WebSocketListener() {
-        override fun onOpen(webSocket: WebSocket, response: Response) {
-            subscribeTopic("/topic/example") // Subscribe to a topic after connection
-        }
+    val stompClient =  Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
 
-        override fun onMessage(webSocket: WebSocket, text: String) {
-            // Handle incoming text messages
-            println("Received: $text")
-        }
-
-        override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
-            // Handle incoming binary messages
-        }
-
-        override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-            // Handle connection failure
-        }
-    }
-
-    private val webSocket = client.newWebSocket(request, listener)
-
-    fun subscribeTopic(topic: String) {
-        val subscribeFrame = "SUBSCRIBE\n" +
-                "id:sub-0\n" +
-                "destination:$topic\n\n"
-
-        webSocket.send(subscribeFrame)
-    }
-
-    fun sendMessage(destination: String, message: String) {
-        val sendFrame = "SEND\n" +
-                "destination:$destination\n\n" +
-                "$message\n"
-
-        webSocket.send(sendFrame)
-    }
-
-    fun disconnect() {
-        webSocket.close(1000, "Disconnecting")
-    }
+//    private val listener = object : WebSocketListener() {
+//        override fun onOpen(webSocket: WebSocket, response: Response) {
+//            subscribeTopic("/topic/example") // Subscribe to a topic after connection
+//        }
+//
+//        override fun onMessage(webSocket: WebSocket, text: String) {
+//            // Handle incoming text messages
+//            println("Received: $text")
+//        }
+//
+//        override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+//            // Handle incoming binary messages
+//        }
+//
+//        override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+//            // Handle connection failure
+//        }
+//    }
+//
+//    private val webSocket = client.newWebSocket(request, listener)
+//
+//    fun subscribeTopic(topic: String) {
+//        val subscribeFrame = "SUBSCRIBE\n" +
+//                "id:sub-0\n" +
+//                "destination:$topic\n\n"
+//
+//        webSocket.send(subscribeFrame)
+//    }
+//
+//    fun sendMessage(destination: String, message: String) {
+//        val sendFrame = "SEND\n" +
+//                "destination:$destination\n\n" +
+//                "$message\n"
+//
+//        webSocket.send(sendFrame)
+//    }
+//
+//    fun disconnect() {
+//        webSocket.close(1000, "Disconnecting")
+//    }
 }
--- app/src/main/java/com/sch/sch_taxi/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sch/sch_taxi/ui/home/HomeViewModel.kt
@@ -1,7 +1,10 @@
 package com.sch.sch_taxi.ui.home
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.sch.data.api.ApiClient
 import com.sch.domain.model.Reservation
 import com.sch.domain.model.Taxi
 import com.sch.domain.model.Taxis
@@ -9,10 +12,18 @@ import com.sch.domain.onError
 import com.sch.domain.onSuccess
 import com.sch.domain.usecase.main.GetReservationUseCase
 import com.sch.sch_taxi.base.BaseViewModel
+import com.sch.sch_taxi.di.PresentationApplication.Companion.sSharedPreferences
+import com.sch.sch_taxi.di.StompNetworkModule
 import com.sch.sch_taxi.ui.home.adapter.createReservationPager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
+import ua.naiksoftware.stomp.Stomp
+import ua.naiksoftware.stomp.dto.LifecycleEvent
+import ua.naiksoftware.stomp.dto.StompHeader
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,9 +40,64 @@ class HomeViewModel @Inject constructor(
 
     var reservationEvent: Flow<PagingData<Reservation>> = emptyFlow()
 
+    private val url = "ws" + ApiClient.BASE_URL.substring(4) + "stomp/chat"
+
+    private val stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
 
     init {
         getReservation()
+
+        Log.d("ttt", url.toString())
+        baseViewModelScope.launch {
+            runStomp()
+        }
+
+    }
+
+    @SuppressLint("CheckResult")
+    fun runStomp() {
+
+        val headerList = arrayListOf<StompHeader>()
+        headerList.add(
+            StompHeader(
+                "Authorization",
+                "Bearer "+ sSharedPreferences.getString("accessToken", null)
+            )
+        )
+        headerList.add(StompHeader("simpSessionId", "1"))
+        stompClient.connect(headerList)
+
+        stompClient.topic("/sub/chat/room/1").subscribe { topicMessage ->
+            Log.i("message Recieve", topicMessage.payload)
+        }
+
+        stompClient.lifecycle().subscribe { lifecycleEvent ->
+            when (lifecycleEvent.type) {
+                LifecycleEvent.Type.OPENED -> {
+                    Log.d("ttt", "OPEND !!")
+                }
+
+                LifecycleEvent.Type.CLOSED -> {
+                    Log.d("ttt", "CLOSED !!")
+
+                }
+
+                LifecycleEvent.Type.ERROR -> {
+                    Log.d("ttt", "ERROR !!")
+                    Log.d("ttt", "CONNECT ERROR" + lifecycleEvent.exception.toString())
+                }
+
+                else -> {
+                    Log.d("ttt",  "ELSE" + lifecycleEvent.message)
+                }
+            }
+        }
+
+        val data = JSONObject()
+        data.put("roomId", 1)
+        data.put("message", "안녕하세요")
+
+        stompClient.send("/pub/chat/message", data.toString()).subscribe()
     }
 
     fun getReservation() {
