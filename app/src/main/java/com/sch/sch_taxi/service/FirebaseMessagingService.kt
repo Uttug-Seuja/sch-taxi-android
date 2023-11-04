9a4aea080a6caa402f4598af251271ef17feeda7
JunJangE <wnswkd486@gmail.com>
2023-11-04 12:22:10 +0900

    fix: 백그라운드 알림 설정

--- app/src/main/java/com/sch/sch_taxi/service/FirebaseMessagingService.kt
+++ app/src/main/java/com/sch/sch_taxi/service/FirebaseMessagingService.kt
@@ -1,100 +1,103 @@
-package com.sch.sch_taxi.service
-
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
-import com.sch.sch_taxi.R
-import com.sch.sch_taxi.ui.MainActivity
-import java.util.*
-
-
-class FirebaseMessagingService : FirebaseMessagingService() {
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-        //token을 서버로 전송
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        super.onMessageReceived(remoteMessage)
-        val notificationManager = NotificationManagerCompat.from(
-            applicationContext
-        )
-        var builder: NotificationCompat.Builder? = null
-        builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (notificationManager.getNotificationChannel("sch_taxi") == null) {
-                val channel = NotificationChannel(
-                    "sch_taxi",
-                    "sch_taxi",
-                    NotificationManager.IMPORTANCE_DEFAULT
-                )
-                notificationManager.createNotificationChannel(channel)
-            }
-            NotificationCompat.Builder(applicationContext, "sch_taxi")
-        } else {
-            NotificationCompat.Builder(applicationContext)
-        }
-        val title = remoteMessage.notification!!.title
-        val body = remoteMessage.notification!!.body
-        builder.setContentTitle(title)
-            .setContentText(body)
-            .setSmallIcon(R.drawable.sch_logo)
-        val notification: Notification = builder.build()
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        notificationManager.notify(1, notification)
-
-//        remoteMessage.notification?.let {
-//            showNotification(it)
+//package com.sch.sch_taxi.service
+//
+//import android.Manifest
+//import android.annotation.SuppressLint
+//import android.app.Notification
+//import android.app.NotificationChannel
+//import android.app.NotificationManager
+//import android.app.PendingIntent
+//import android.content.Intent
+//import android.content.pm.PackageManager
+//import android.os.Build
+//import android.util.Log
+//import androidx.annotation.RequiresApi
+//import androidx.core.app.ActivityCompat
+//import androidx.core.app.NotificationCompat
+//import androidx.core.app.NotificationManagerCompat
+//import com.google.firebase.messaging.FirebaseMessagingService
+//import com.google.firebase.messaging.RemoteMessage
+//import com.sch.sch_taxi.R
+//import com.sch.sch_taxi.ui.MainActivity
+//import java.util.*
+//
+//
+//class FirebaseMessagingService : FirebaseMessagingService() {
+//    override fun onNewToken(token: String) {
+//        super.onNewToken(token)
+//        //token을 서버로 전송
+//    }
+//
+//    @RequiresApi(Build.VERSION_CODES.M)
+//    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+//        super.onMessageReceived(remoteMessage)
+//        val notificationManager = NotificationManagerCompat.from(
+//            applicationContext
+//        )
+//        var builder: NotificationCompat.Builder? = null
+//        builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            if (notificationManager.getNotificationChannel("sch_taxi") == null) {
+//                val channel = NotificationChannel(
+//                    "sch_taxi",
+//                    "sch_taxi",
+//                    NotificationManager.IMPORTANCE_DEFAULT
+//                )
+//                notificationManager.createNotificationChannel(channel)
+//            }
+//            NotificationCompat.Builder(applicationContext, "sch_taxi")
+//        } else {
+//            NotificationCompat.Builder(applicationContext)
 //        }
-    }
-
-    @SuppressLint("UnspecifiedImmutableFlag")
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun showNotification(notification: RemoteMessage.Notification) {
-        val intent = Intent(this, MainActivity::class.java)
-        val pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val channelId = "sch_taxi"
-
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setSmallIcon(R.drawable.sch_logo)
-            .setContentTitle(notification.title)
-            .setContentText(notification.body)
-            .setContentIntent(pIntent)
-
-        getSystemService(NotificationManager::class.java).run {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val channel = NotificationChannel(channelId, "알림", NotificationManager.IMPORTANCE_HIGH)
-                createNotificationChannel(channel)
-            }
-
-            notify(Date().time.toInt(), notificationBuilder.build())
-        }
-    }
-}
+//
+//        Log.d("ttt", remoteMessage.messageType.toString())
+//        val title = remoteMessage.notification!!.title
+//        val body = remoteMessage.notification!!.body
+//        builder.setContentTitle(title)
+//            .setContentText(body)
+//            .setSmallIcon(R.drawable.sch_logo)
+//        val notification: Notification = builder.build()
+//        if (ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.POST_NOTIFICATIONS
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return
+//        }
+//        notificationManager.notify(1, notification)
+//
+////        remoteMessage.notification?.let {
+////            showNotification(it)
+////        }
+//    }
+//
+//    @SuppressLint("UnspecifiedImmutableFlag")
+//    @RequiresApi(Build.VERSION_CODES.M)
+//    private fun showNotification(notification: RemoteMessage.Notification) {
+//        val intent = Intent(this, MainActivity::class.java)
+//        val pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//
+//        val channelId = "sch_taxi"
+//
+//        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+//            .setPriority(NotificationCompat.PRIORITY_HIGH)
+//            .setSmallIcon(R.drawable.sch_logo)
+//            .setContentTitle(notification.title)
+//            .setContentText(notification.body)
+//            .setContentIntent(pIntent)
+//
+//        getSystemService(NotificationManager::class.java).run {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                val channel = NotificationChannel(channelId, "알림", NotificationManager.IMPORTANCE_HIGH)
+//                createNotificationChannel(channel)
+//            }
+//
+//            notify(Date().time.toInt(), notificationBuilder.build())
+//        }
+//    }
+//}
