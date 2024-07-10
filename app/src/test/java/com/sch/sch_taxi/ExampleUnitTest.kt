57179e23137f8c5a5d257e8b75aedba8649695af
junjange <wnswkd486@naver.com>
2024-07-11 01:33:08 +0900

    test: test 코드 추가

--- app/src/test/java/com/sch/sch_taxi/ExampleUnitTest.kt
+++ app/src/test/java/com/sch/sch_taxi/ExampleUnitTest.kt
@@ -14,4 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+
+    @Test
+    fun addition_isCorrect222() {
+        assertEquals(4, 2 + 2)
+    }
 }
